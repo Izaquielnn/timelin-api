8ecd82a84ceb9edd10a6a48d3e8b092a966b97c1
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2019-03-15 18:24:16 -0300

    add user controller

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require('../models/user');
+
+module.exports = {
+    async store(req, res) {
+        const {name, email, password} = req.body;
+        let user = User.build({name, email, password});
+        user.validate().catch( (err) => {
+            return res.status(400).send(err.errors[0].message);
+        });
+
+        user = await  User.findOne({ where: { email }});
+        if(user) return res.status(400).send('Usuário já cadastrado.');
+        
+        const newUser = await User.create({name, email, password});
+        
+        user = {
+            id: newUser.id,
+            name: newUser.name,
+            email: newUser.email,
+        }
+        res.send(user);
+    }
+}
