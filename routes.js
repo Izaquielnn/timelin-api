ea3ad72b13bd2ca10a80bc6408e5f0aeeb3ba0ee
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2019-03-16 14:13:57 -0300

    add tags routes

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ const routes = express.Router();
 const userController = require('./controllers/userController');
 const authController = require('./controllers/authController');
 const eventController = require('./controllers/eventController');
+const tagController = require('./controllers/tagController');
 
 routes.post('/login', authController.login);
 
@@ -16,5 +17,10 @@ routes.put('/users/me', auth, userController.update);
 routes.post('/events', auth, eventController.store);
 routes.get('/events', auth, eventController.index);
 routes.put('/events/:id', auth, eventController.update);
+
+routes.post('/tags', auth, tagController.store);
+routes.get('/tags', auth, tagController.index);
+routes.put('/tags/:id', auth, tagController.update);
+routes.delete('/tags/:id', auth, tagController.delete);
 
 module.exports = routes;
