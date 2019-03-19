4bb79383a3a616ab319bb5a02dbbb53d1e754d41
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2019-03-19 09:59:23 -0300

    remove force drop tables

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-db.sync({ force: true }).then(() => {
+db.sync().then(() => {
 
     console.log("database connected...");
     app.listen(PORT, console.log(`Server started on port ${PORT}`));
